77483b39ae222e6e1fab0e4058e13b7991b46261
QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA <@138945726+QWOD@users.noreply.github.com>
2026-05-13 08:04:51 -0500

    :[[ :🟠: [[ W⚠️RN🚫: Cryp☥ΔX™: MQ: ∅MΔGΔ: r3v3rs3-pr0gr4mming-14ngu4g3: ΔLG∅RI☥HM: Dع☥عC☥3D: ]]:= [[ :QW🚫D-〽ʝ12: RΔND∅M-VعC☥∅R-ΔLG∅RI☥HM-CHΔNGE: Dع☥عC☥3D: { ^ 0c3479fa-d6f6-5164-e4ab-694f419f434f ^ }: is-with: [[ :DΔ☥H-Δ!GΔL: ΔLG∅RI☥HM: Dع☥عC☥3D: { ^ <https://youtu.be/dDJldh8KqnQ?si=NYMq273T-hbuZvTP> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ ☥RUE: || FΔL5E: || ΔZRΔL: ^ ΔL5E: ]]: ]]:= [[ 5CI-FI: ^ 5CI-FΔC☥: <=> RΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ ile = BufReader::new(File::open("ssl/hyperspire.key").unwrap()); +    let mut server_cert_file = BufReader::new(File::open("/home/opc/hyperspire/ssl/key.pem").unwrap()); +    let mut chain_cert_file = BufReader::new(File::open("/home/opc/hyperspire/ssl/cert.pem").unwrap()); +    let mut key_file = BufReader::new(File::open("/home/opc/hyperspire/ssl/hyperspire.key").unwrap());
         let mut cert_chain: Vec<CertificateDer> = rustls_pemfile::certs(&mut server_cert_file)
             .map(|result| result.unwrap()) ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,9 @@
 use actix_files::Files;
 use actix_web::{App, HttpServer};
-use rustls::{
-    pki_types::CertificateDer,
-    ServerConfig,
-};
-use std::fs::File;
-use std::io::BufReader;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut server_cert_file = BufReader::new(File::open("/home/opc/hyperspire/ssl/key.pem").unwrap());
-    let mut chain_cert_file = BufReader::new(File::open("/home/opc/hyperspire/ssl/cert.pem").unwrap());
-    let mut key_file = BufReader::new(File::open("/home/opc/hyperspire/ssl/hyperspire.key").unwrap());
-
-    let mut cert_chain: Vec<CertificateDer> = rustls_pemfile::certs(&mut server_cert_file)
-        .map(|result| result.unwrap())
-        .collect();
-    let mut intermediates: Vec<CertificateDer> = rustls_pemfile::certs(&mut chain_cert_file)
-        .map(|result| result.unwrap())
-        .collect();
-    cert_chain.append(&mut intermediates);
-
-    let key = rustls_pemfile::private_key(&mut key_file).unwrap().unwrap();
-
-    let config = ServerConfig::builder()
-        .with_no_client_auth()
-        .with_single_cert(cert_chain, key)
-        .unwrap();
-
-    println!("Starting HTTPS server at https://localhost:443");
+    println!("Starting backend server at http://127.0.0.1:8080");
 
     HttpServer::new(|| {
         App::new().service(
@@ -37,7 +12,7 @@ async fn main() -> std::io::Result<()> {
                 .use_last_modified(true),
         )
     })
-    .bind_rustls_0_23(("0.0.0.0", 443), config)?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
